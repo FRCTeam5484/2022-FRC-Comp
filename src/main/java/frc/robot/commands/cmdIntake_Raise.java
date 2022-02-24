84fd6df524d676824cdbb687427a2ad517ee7160
RogerMCT <rogermct@hotmail.com>
2022-02-24 18:04:32 -0500

    Intake

--- src/main/java/frc/robot/commands/cmdIntake_Raise.java
+++ src/main/java/frc/robot/commands/cmdIntake_Raise.java
@@ -1,33 +1,12 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.subIntakeSystem;
 import frc.robot.subsystems.subPneumaticSystem;
 
-public class cmdIntake_Raise extends CommandBase {
-  subPneumaticSystem air;
-  subIntakeSystem intake;
-  public cmdIntake_Raise(subPneumaticSystem _air, subIntakeSystem _intake) {
-    air = _air;
-    intake = _intake;
-    addRequirements(air);
-    addRequirements(intake);
-  }
-
-  @Override
-  public void initialize() {
-    air.raiseIntake();
-    intake.stopIntake();
-  }
-
-  @Override
-  public void execute() {}
-
-  @Override
-  public void end(boolean interrupted) { }
-
-  @Override
-  public boolean isFinished() {
-    return false;
+public class cmdIntake_Raise extends SequentialCommandGroup  {
+  public cmdIntake_Raise(subPneumaticSystem air, subIntakeSystem intake) {
+    addCommands(new InstantCommand(() -> air.raiseIntake()), new cmdIntake_RunSeconds(intake));
   }
 }
---
+++ src/main/java/frc/robot/commands/cmdIntake_RunSeconds.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.subIntakeSystem;
+
+public class cmdIntake_RunSeconds extends CommandBase {
+  subIntakeSystem intake;
+  Timer time;
+  public cmdIntake_RunSeconds(subIntakeSystem _intake) {
+    intake = _intake;
+    addRequirements(intake);
+  }
+
+  @Override
+  public void initialize() {
+    time = new Timer();
+    time.start();
+  }
+
+  @Override
+  public void execute() {
+    intake.runIntake();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intake.stopIntake();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return time.get() > 1 ? true : false;
+  }
+}
